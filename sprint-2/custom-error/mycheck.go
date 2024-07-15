a20cacb73b2690fd92802da4aaa3858b14b8f1e1
netzen86 <netzen0su@gmail.com>
2024-07-15 21:28:19 +0500

    task layout

--- sprint-2/custom-error/mycheck.go
+++ sprint-2/custom-error/mycheck.go
@@ -2,6 +2,19 @@
 
 package mycheck
 
+type CheckError struct {
+	Errors []error
+}
+
+func (se CheckError) Error() []error {
+	return se.Errors
+}
+
+func Error(text string) CheckError {
+	var result CheckError
+
+}
+
 func MyCheck(input string) error {
 	// TODO
 	return nil
